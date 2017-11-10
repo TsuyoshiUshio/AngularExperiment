160c94e450e28c2ee674bc032628863611c496c3
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-11-10 16:34:44 +0900

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  cars = [];
+  constructor() {
+    this.cars = JSON.parse(localStorage.getItem("cars")) || [];
+    console.log('this.cars:' + this.cars);
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+    this.cars = [{"name": "subaru"}];
+    localStorage.setItem("cars", JSON.stringify(this.cars));
+    }, 3000)
+
+  }
 }
